1e5c0a1503693d90df958c04dbb8027af43bfccf
Austin Riba <austin@m51.io>
2026-03-16 12:44:44 -0700

    Add method to subscribe with an event filter.

--- src/connection.rs
+++ src/connection.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 
 use log::warn;
 use tokio::sync::broadcast::error::RecvError;
+use tokio::sync::mpsc;
 
 use crate::commands::Command;
 use crate::error::Error;
@@ -37,6 +38,7 @@ impl<T: Transport> Connection<T> {
         })
     }
 
+    /// Execute a one-shot command and wait for a matching response (or error) with a timeout.
     pub async fn execute<C: Command + Send>(
         &self,
         cmd: C,
@@ -64,7 +66,41 @@ impl<T: Transport> Connection<T> {
         .map_err(|_| Error::Timeout)?
     }
 
-    pub fn subscribe(&self) -> tokio::sync::broadcast::Receiver<Event> {
+    /// Subscribe to events matching a predicate function.
+    ///
+    /// Returns a receiver that only returns events matching the
+    /// supplied predicate. This prevents backpressure
+    /// from building up in the event bus from events that the caller doesn't care about.
+    /// Note that device errors are ignored - these could be from other commands.
+    /// If the caller wants to be notified of device errors, they should add them
+    /// to the predicate.
+    pub fn subscribe<F>(&self, predicate: F) -> mpsc::Receiver<Event>
+    where
+        F: Fn(&Event) -> bool + Send + 'static,
+    {
+        let mut broadcast_rx = self.event_bus.subscribe();
+        let (tx, rx) = mpsc::channel(16);
+
+        tokio::spawn(async move {
+            loop {
+                match broadcast_rx.recv().await {
+                    Ok(event) if predicate(&event) => {
+                        if tx.send(event).await.is_err() {
+                            break;
+                        }
+                    }
+                    Ok(_) => continue,
+                    Err(RecvError::Lagged(n)) => warn!("subscription lagged by {n} events"),
+                    Err(RecvError::Closed) => break,
+                }
+            }
+        });
+
+        rx
+    }
+
+    /// Subscribe to all events without filtering.
+    pub fn subscribe_all(&self) -> tokio::sync::broadcast::Receiver<Event> {
         self.event_bus.subscribe()
     }
 }
